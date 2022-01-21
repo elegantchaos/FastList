9b8fa31995e71b6eedf9bf3981997d5877167ece
Sam Deane <sam@elegantchaos.com>
2022-01-21 12:49:35 +0000

    first version

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # FastList
 
-A description of this package.
+A fast but very bare-bones replacement for SwiftUI's List.
+
+This is intended to work-around some of List's problems, especially with regard to large lists, which suffer terribly from SwiftUI's attempts to animate updates in a "clever" way :(.
+
+It is intended as a drop-in replacement, but right now it doesn't even pretend to do 90% of what List does - it just does what I need it to do in the project(s) I wrote it for.
+
+I will expand it as I need other features, and I'm open to pull-requests which add (for example), support for other list styles, as long as they don't break performance. 
--- Sources/FastList/FastList.swift
+++ Sources/FastList/FastList.swift
@@ -3,6 +3,30 @@
 //  All code (c) 2022 - present day, Elegant Chaos.
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
-struct FastList {
-    var text = "Hello, World!"
+import SwiftUI
+
+struct FastList<Data, Content>: View where Data: RandomAccessCollection, Content: View, Data.Element: Identifiable {
+    @Environment(\.lineSpacing) var lineSpacing
+    
+    public var data: Data
+    public var content: (Data.Element) -> Content
+
+    public init(_ data: Data, @ViewBuilder content: @escaping (Data.Element) -> Content) {
+        self.data = data
+        self.content = content
+    }
+
+    var body: some View {
+        ScrollView {
+            LazyVStack(alignment: .leading, spacing: lineSpacing) {
+                ForEach(data) { item in
+                    content(item)
+                    Divider()
+                }
+            }
+        }
+        .foregroundColor(.primary)
+        .padding(.horizontal)
+    }
+    
 }
