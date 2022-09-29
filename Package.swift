0a087b36291778d7749d96ebb107882954163531
Sam Deane <sam@elegantchaos.com>
2022-09-29 15:11:11 +0100

    Added iOS 16.0 support to fix problem with double refresh indicators.

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
             targets: ["FastList"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/elegantchaos/RefreshableScrollView.git", from: "1.1.7"),
+        .package(url: "https://github.com/elegantchaos/RefreshableScrollView.git", from: "1.3.0"),
         .package(url: "https://github.com/elegantchaos/XCTestExtensions.git", from: "1.4.2")
     ],
     targets: [
